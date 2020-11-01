fc8bdf395c4bba71e2a9ef32c7c44c356acac0d7
Milko Yanakiev <myanakiev@gmail.com>
2020-11-01 12:35:59 +0200

    Git setup.

---
+++ src/HR_DS/Solution.java
@@ -0,0 +1,68 @@
+package HR_DS;
+
+import java.io.*;
+import java.util.*;
+
+// https://www.hackerrank.com/challenges/2d-array/problem?h_r=next-challenge&h_v=zen
+
+public class Solution {
+    static int hourglassSum(int[][] arr) {
+
+        int result = 0;
+
+        for (int row = 0; row <= 3; row++) {
+            for (int col = 0; col <= 3; col++) {
+
+                int current = checkHourglass(arr, row, col);
+
+                if (current > result) {
+                    result = current;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private static int checkHourglass(int[][] arr, int row, int col) {
+
+        int res = 0;
+
+        for (int i = row; i < row + 3; i++) {
+            for (int j = col; j < col + 3; j++) {
+                res += arr[i][j];
+            }
+        }
+
+        res = res - arr[row + 1][col] - arr[row + 1][col + 2];
+
+        return res;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int[][] arr = new int[6][6];
+
+        for (int i = 0; i < 6; i++) {
+            String[] arrRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 6; j++) {
+                int arrItem = Integer.parseInt(arrRowItems[j]);
+                arr[i][j] = arrItem;
+            }
+        }
+
+        int result = hourglassSum(arr);
+
+//        System.out.println(result);
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
