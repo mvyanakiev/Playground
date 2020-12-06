041ea9fa3d5f013f0220d1c52e23df2198434d07
Milko Yanakiev <myanakiev@gmail.com>
2020-12-06 11:45:16 +0200

    Change message in Config.java.

--- src/type1/Config.java
+++ src/type1/Config.java
@@ -5,5 +5,5 @@ class Config {
 
     static final String DATA_FORMAT = "yyyy-MM-dd";
 
-    static final String NO_ANSWER_MESSAGE = "There is no project on the set parameters!";
+    static final String NO_ANSWER_MESSAGE = "There is no project on the set parameters!!!";
 }
