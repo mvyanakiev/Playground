653c2838ec27ccbae56601735ac0adb723ca19d6
Milko Yanakiev <myanakiev@gmail.com>
2020-12-06 12:06:37 +0200

    Fx.

--- src/type1/Config.java
+++ src/type1/Config.java
@@ -3,7 +3,7 @@ package type1;
 class Config {
     static final String FILE_PATH = "src\\inputData\\testData.txt";
 
-    static final String DATA_FORMAT =  "yyyy-MM-dd";
+    static final String  DATA_FORMAT = "yyyy-MM-dd";
 
     static final String NO_ANSWER_MESSAGE = "There is no project on the set parameters!!!";
 }
