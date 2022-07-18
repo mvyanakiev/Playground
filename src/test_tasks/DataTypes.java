4745b49fca9037b00e0f103a115f729d98424068
Milko Yanakiev <myanakiev@degiro.nl>
2022-07-18 09:10:52 +0300

    Data types.

---
+++ src/test_tasks/DataTypes.java
@@ -0,0 +1,14 @@
+package test_tasks;
+
+import java.util.Date;
+
+public class DataTypes {
+    public static void main(String[] args) {
+
+        Date date = new Date();
+
+        System.out.println(date);
+        date.setDate(1);
+        System.out.println(date);
+    }
+}
